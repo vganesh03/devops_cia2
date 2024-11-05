7fbe8bdfcfe2ab9b01261734fcfba993abe96ac2
Varun Ganesh <89720334+vganesh03@users.noreply.github.com>
2024-11-05 11:07:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get("/rest/getAllUsers", (req, res) => {
 
 // Add Hello World route
 app.get("/", (req, res) => {
-  res.send("Hello There!");
+  res.send("Hello World!");
 });
 
 // Start the server
